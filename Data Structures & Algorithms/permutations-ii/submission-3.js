4da91e2969713ffa25e526a1ef40110d4df4ac90
Madhav <64889547+MadhavVankina@users.noreply.github.com>
2026-04-20 09:08:58 +0530

    Add: permutations-ii - submission-3

---
+++ Data Structures & Algorithms/permutations-ii/submission-3.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+
+    // [1,1,2]
+    // p = [1,2] [2, 1]
+    // res => [1,1,2] [1,1,2] [1,2,1] [1,2,1] [2,1,1] [2,1,1] 
+    // --------
+    // [1,2]
+    // p -> [2]
+    // res => [1,2] [2, 1]
+    // --------
+    // [2]
+    // p -> []
+    // res => [[]]
+
+    permuteUnique(nums) {
+        nums.sort((a, b) => b - a)
+        const res = this.backtrack(nums)
+
+        return res;
+        
+    }
+
+    backtrack(nums){
+        if(nums.length === 0) return [[]]
+        const perms = this.backtrack(nums.slice(1));
+        const res = [];
+
+        for(let p of perms){
+            let prev = -11;
+            for(let i = 0; i <= p.length; i++){
+                if(nums[0] === prev) continue;
+                const p_copy = p.slice();
+                p_copy.splice(i, 0, nums[0]);
+                res.push(p_copy);
+                prev = p[i];
+            }
+        }
+
+        return res;
+    }
+}
