0182505d7c6dc9d6eaa1bbfd19385b4a51e71410
Madhav <64889547+MadhavVankina@users.noreply.github.com>
2026-04-20 15:33:17 +0530

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.js
@@ -0,0 +1,56 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string[][]}
+     */
+    
+    // aab
+    // [a, a, b]
+    // [aa, b]
+    // [a, ab]
+    // [aab]
+
+    // [a] 
+    // [a, a] [aa]
+    // [a, a, b] [a, ab] [aa, b] [aab]
+
+
+
+    partition(s) {
+        const res = [];
+
+        const substring = (curr, i) => {
+            if(s.length == i) {
+                for(let i = 0; i < curr.length; i++){
+                    if(!this.isPalindrome(curr[i])) return;
+                }
+                res.push([...curr]);
+                return;
+            }
+
+            curr.push(s[i]);
+            substring(curr, i + 1);
+            curr.pop();
+            curr[curr.length - 1] = curr[curr.length - 1] + s[i];
+            substring(curr, i + 1);
+
+        }
+
+        if(s.length == 0) return []
+
+        substring([...s[0]], 1);
+
+        return res;
+    }
+
+
+    isPalindrome(s){
+        let [l, r] = [0, s.length - 1];
+        while(l < r) {
+            if(s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
