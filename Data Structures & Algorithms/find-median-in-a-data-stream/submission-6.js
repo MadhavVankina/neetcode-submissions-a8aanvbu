4fad0cf0e5b48ad9416233d7368f9d9689bc6b81
Madhav <64889547+MadhavVankina@users.noreply.github.com>
2026-05-16 13:25:41 +0530

    Add: find-median-in-a-data-stream - submission-6

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-6.js
@@ -0,0 +1,43 @@
+class MedianFinder {
+    constructor() {
+        this.minQ = new MinPriorityQueue();
+        this.maxQ = new MaxPriorityQueue();
+    }
+
+    /**
+     *
+     * @param {number} num
+     * @return {void}
+     */
+
+    // [1, 2, 3, 4, 5]
+    //  max.      min. 
+    //  [1, 3, 2]
+
+    //  [1] []
+    //  [1] [3]
+    //   
+
+    addNum(num) {
+        this.minQ.enqueue(num)
+
+        if(this.minQ.size() > this.maxQ.size()){
+            const curr = this.minQ.dequeue()
+            this.maxQ.enqueue(curr);
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    findMedian() {
+        if ((this.maxQ.size() + this.minQ.size()) % 2 === 0){
+            const m1 = this.maxQ.front()
+            const m2 = this.minQ.front()
+
+            return (m1 + m2) / 2
+        }
+
+        return  this.maxQ.front()
+    }
+}
