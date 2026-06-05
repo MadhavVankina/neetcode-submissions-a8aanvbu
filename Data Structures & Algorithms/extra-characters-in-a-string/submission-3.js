c0327375c35375c7c7164f6a02257ed3589f9718
Madhav <64889547+MadhavVankina@users.noreply.github.com>
2026-06-05 11:47:59 +0530

    Add: extra-characters-in-a-string - submission-3

---
+++ Data Structures & Algorithms/extra-characters-in-a-string/submission-3.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string[]} dictionary
+     * @return {number}
+     */
+
+    // leetscode
+    // 1
+
+    minExtraChar(s, dictionary) {
+        const dp = {};
+
+        const backtrack = (i) => {
+            if(i == s.length) return 0;
+
+            if (dp[i]) return dp[i];
+
+            let res = 1 + backtrack(i + 1);
+
+            for (let w of dictionary) {
+                const j = w.length;
+
+                if (i + j <= s.length && s.slice(i, i + j) === w) {
+                    res = Math.min(res, backtrack(i + j));
+                }
+            }
+
+            dp[i] = res;
+            return res;
+        };
+
+        return backtrack(0);
+    }
+}
