54be9a7e1af2553e32702a6ba46ad2611b9b9a34
Madhav <64889547+MadhavVankina@users.noreply.github.com>
2026-06-09 18:43:07 +0530

    Add: island-perimeter - submission-1

---
+++ Data Structures & Algorithms/island-perimeter/submission-1.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    // Since there is only one island dfs is effective
+    // r, c
+    // base case?
+    // if(r < 0 || c < 0 || r == row || c == col || grid[r][c] == 0) -> +1 return;
+    // 
+    islandPerimeter(grid) {
+        let res = 0;
+        const row = grid.length;
+        const col = grid[0].length;
+
+        const dfs = (r, c) => {
+            if(r < 0 || c < 0 || r == row || c == col || grid[r][c] == 0) {
+                res++;
+                return;
+            }
+
+            if(grid[r][c] == '#'){
+                return;
+            }
+
+            grid[r][c] = '#';
+
+            dfs(r + 1, c);
+            dfs(r - 1, c);
+            dfs(r, c + 1);
+            dfs(r, c - 1);
+        }
+
+        for(let r = 0; r < row; r++){
+            for(let c = 0; c < col; c++){
+                if(grid[r][c] == 1){
+                    dfs(r, c);
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+}
