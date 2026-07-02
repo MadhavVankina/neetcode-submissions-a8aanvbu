e43365890110b7ce8044c79fb120aa31793f3464
Madhav <64889547+MadhavVankina@users.noreply.github.com>
2026-07-02 15:11:54 +0530

    Add: course-schedule - submission-10

---
+++ Data Structures & Algorithms/course-schedule/submission-10.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        const adjMap = Array.from({ length: numCourses }, () => []);
+        const visited = new Set();
+
+        for (let [a, b] of prerequisites) {
+            adjMap[a].push(b);
+        }
+
+        const DFS = (n) => {
+            if (visited.has(n)) return false;
+
+            if (adjMap[n].length == 0) return true;
+            visited.add(n);
+
+            for (let a of adjMap[n]) {
+                if (!DFS(a)) return false;
+            }
+
+            visited.delete(n);
+            adjMap[n] = [];
+            return true;
+        };
+
+        for (let i = 0; i < numCourses; i++) {
+            if (!DFS(a)) return false;
+        }
+
+        return true;
+    }
+}
