342e9398571118d4de53063471ef7c6a3438e6cf
Madhav <64889547+MadhavVankina@users.noreply.github.com>
2026-07-07 12:37:21 +0530

    Add: course-schedule-iv - submission-8

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-8.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @param {number[][]} queries
+     * @return {boolean[]}
+     */
+    // 1 -> 2 -> 0
+    // 1 ->   -> 0
+    checkIfPrerequisite(numCourses, prerequisites, queries) {
+        const adjmap = Array.from( { length: numCourses }, () => []);
+        const result = [];
+        const visited = new Set();
+
+        for(let [a, b] of prerequisites){
+            adjmap[a].push(b);
+        }
+
+        const DFS = (curr, target) => {
+            if(curr === target) return true;
+            if(visited.has(curr)) return false;
+
+            visited.add(curr);
+
+            for(let a of adjmap[curr]){
+                if(DFS(a, target)) {
+                    visited.delete(a);
+                    return true;
+                } 
+            }
+
+            visited.delete(curr);
+            return false;
+        }
+
+        for(let [curr, target] of queries){
+            result.push(DFS(curr, target));
+        }
+
+        return result;
+    }
+}
