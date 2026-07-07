08ed9b9ea2e8bb190a0c61be290c63702e4b071e
Madhav <64889547+MadhavVankina@users.noreply.github.com>
2026-07-07 18:30:22 +0530

    Add: count-connected-components - submission-5

---
+++ Data Structures & Algorithms/count-connected-components/submission-5.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n, edges) {
+        const adjmap = Array.from({ length: n }, () => []);
+        const visited = new Set();
+        let count = 0;
+
+        for (let [a, b] of edges) {
+            adjmap[a].push(b);
+            adjmap[b].push(a);
+        }
+
+        const DFS = (curr) => {
+            if (visited.has(curr)) return;
+
+            visited.add(curr);
+
+            for(let a of adjmap[curr]){
+                DFS(a);
+            }
+        }
+
+        for(let i = 0; i < n; i++){
+            if(!visited.has(i)){
+                DFS(i);
+                count++;
+            }
+        }
+
+
+        return count;
+    }
+}
