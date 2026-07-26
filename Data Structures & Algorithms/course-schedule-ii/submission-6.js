3460493be31061c3153f9b5e102064ac8a427718
Madhav <64889547+MadhavVankina@users.noreply.github.com>
2026-07-26 23:36:01 +0530

    Add: course-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-6.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {number[]}
+     */
+    findOrder(numCourses, prerequisites) {
+        const adj = Array.from({ length: numCourses }, () => []);
+        const cycle = new Set();
+        const visit = new Set();
+        const result = [];
+
+        prerequisites.forEach(([a, b]) => adj[a].push(b));
+
+        const dfs = (node) => {
+            if (cycle.has(node)) return false;
+            if (visit.has(node)) return true;
+
+            cycle.add(node);
+            for (let nei of adj[node]) {
+                if (!dfs(nei)) return false;
+            }
+
+            cycle.delete(node);
+            visit.add(node);
+            result.push(node);
+            return true;
+        };
+
+        for (let i = 0; i < numCourses; i++) {
+            if (!dfs(i)) return [];
+        }
+
+        return result;
+    }
+}
