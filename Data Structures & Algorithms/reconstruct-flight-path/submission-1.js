4e83b2ee23a7a858809e2185355aa85c0efefcc5
Madhav <64889547+MadhavVankina@users.noreply.github.com>
2026-09-03 23:00:57 +0530

    Add: reconstruct-flight-path - submission-1

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-1.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string[][]} tickets
+     * @return {string[]}
+     */
+    findItinerary(tickets) {
+        const adj = {};
+
+        tickets.sort();
+        for(let [src, dst] of tickets){
+            if(!adj[src]) adj[src] = [];
+            adj[src].push(dst);
+        }
+
+        
+        const result = ['JFK'];
+
+        const dfs = (src) => {
+            if(result.length == tickets.length + 1) return true;
+
+            if(adj[src].length === 0) return false;
+
+            const temp = [...adj[src]];
+            for(let i = 0; i < temp.length; i++){
+                const v = temp[i];
+                adj[src].splice(i, 1);
+                result.push(v);
+                if(dfs(v)) return true;
+                result.pop();
+                adj[src].splice(i, 0, v);
+            }
+
+            return false;
+        }
+
+        dfs('JFK');
+        return result;
+
+    }
+}
